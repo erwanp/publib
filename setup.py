aa8f35141534dcf723fab604a2a8380b1200f0f4
Erwan - Linux <erwan.pannier@ecp.fr>
2016-10-04 14:06:15 +0200

    PEP 8

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import codecs
 long_description = 'Convert a Python expression in a LaTeX formula'
 if os.path.exists('README.rst'):
     long_description = codecs.open('README.rst', encoding="utf-8").read()
-        
+
 setup(name='publib',
       version='0.1.9',
       description='Produce publication-level quality images on top of Matplotlib',
-    	long_description=long_description,
+      long_description=long_description,
       url='https://github.com/erwanp/publib',
       author='Erwan Pannier',
       author_email='erwan.pannier@gmail.com',
@@ -17,18 +17,19 @@ setup(name='publib',
       packages=['publib'],
       install_requires=[
           'matplotlib>=1.4.1',
-          'numpy>=1.9.2', # for testing only. Should make this an optional requirement. 
-		  ],
+          # for testing only. Should make this an optional requirement.
+          'numpy>=1.9.2',
+      ],
       classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Text Processing',
-        'Programming Language :: Python',
-        #'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        "Operating System :: OS Independent"],
-	  include_package_data=True,
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Text Processing',
+          'Programming Language :: Python',
+          #'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          "Operating System :: OS Independent"],
+      include_package_data=True,
       zip_safe=False)
